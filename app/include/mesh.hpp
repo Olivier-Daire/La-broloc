b82f856af3b661ae0409f173fbb84610e826877a
Olivier-Daire <daire.olivier@gmail.com>
2015-12-11 16:21:17 +0100

    Add function draw inside mesh

--- app/include/mesh.hpp
+++ app/include/mesh.hpp
@@ -5,20 +5,20 @@
 #include <string>
 #include "texture.hpp"
 #include "vertex.hpp"
+#include "shader.hpp"
 
 using namespace std;
 
 class Mesh {
     public:
-        /*  Mesh Data  */
         vector<Vertex> vertices;
         vector<GLuint> indices;
         vector<Texture> textures;
-        /*  Functions  */
+        
         Mesh(vector<Vertex> vertices, vector<GLuint> indices, vector<Texture> textures);
+        void Draw(Shader shader);
     private:
-        /*  Render data  */
         GLuint VAO, VBO, EBO;
-        /*  Functions    */
+        
         void setupMesh();
 };  
