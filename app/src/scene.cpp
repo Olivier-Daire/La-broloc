2e1ec5b0e56c8774219957ab8e7e0043c17975dc
bettina <bettina.migot@gmail.com>
2016-01-11 01:55:41 +0100

    Clean scene.cpp

--- app/src/scene.cpp
+++ app/src/scene.cpp
@@ -359,7 +359,6 @@ void Scene::drawLightWall(Shader shader){
 }
 
 void Scene::deleteRoom(){
-    cleanDialogue();
     glDeleteBuffers(1, &_vbo);
     glDeleteVertexArrays(1, &_vao);
 	glDeleteTextures(3, _texturesArray);
